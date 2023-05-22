cb5686a482712357db16773038b15b646b07402c
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-05-22 14:18:40 +0300

    Update description.js

--- oauth2/facebook/description.js
+++ oauth2/facebook/description.js
@@ -1,1 +1,15 @@
-/***/
+/**
+Firstly, go to the facebook console:
+  https://developers.facebook.com/apps/?show_reminder=true
+after this, push the button "<< create app >>"
+choose "<< Set up Facebook log in >>" and << Next...>>
+choose << Website >> and << No, I'm not building a game >>  and "Next.."
+enter "Add an app name" - the name of your app.
+Push "Create app.."
+***
+After this you have been redirected to the "Dashboard"
+In the dashboard push "Products" (on the left - ignition lamp icon)
+Push a dropdown -"Configure->Settings"
+Enter "Valid OAuth Redirect URIs" - for example https://localhost/users/fb"
+
+*/
