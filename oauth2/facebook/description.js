8b12724dd5bb3e0ae189cdfc5845175cb188addb
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-05-22 15:10:01 +0300

    Update description.js

--- oauth2/facebook/description.js
+++ oauth2/facebook/description.js
@@ -11,5 +11,17 @@ After this you have been redirected to the "Dashboard"
 In the dashboard push "Products" (on the left - ignition lamp icon)
 Push a dropdown -"Configure->Settings"
 Enter "Valid OAuth Redirect URIs" - for example https://localhost/users/fb"
+Enter an another extra params - if it`s necessary.
+Push "Settings->Basic" on the left side.
+Get appID and the secret.
+
+
+/*In the app:
+1)Send to the FB server a GET request with URI parameters (clientID and redirect URL);
+2)Receive a response with code;
+3)Sending GETrequest with URI params = clientID, client_secret 
+4)receive an access token
+
+*/
 
 */
